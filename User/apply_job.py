7c9804a87b59e1a96c9d038f31022fe1c850e5f6
Jonaz <worldsstupidestleftpostforward@gmail.com>
2025-02-06 10:03:57 +0800

    openai erroer

--- User/apply_job.py
+++ User/apply_job.py
@@ -1,1 +1,16 @@
-print('i love james')
+import os
+from openai import OpenAI
+
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),  # This is the default and can be omitted
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Say this is a test",
+        }
+    ],
+    model="gpt-4o",
+)
