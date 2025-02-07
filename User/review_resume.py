e5b23bdd13e6c4a861a019453447dd83d2ac1dfb
Jonaz <worldsstupidestleftpostforward@gmail.com>
2025-02-07 08:27:30 +0800

    Created ENV file for API key and fed it to openAI

--- User/review_resume.py
+++ User/review_resume.py
@@ -1,16 +1,28 @@
 import os
-from openai import OpenAI
+from dotenv import load_dotenv
+from pathlib import Path
+from openai import OpenAI  
+
+# Define the absolute path to your .env file
+dotenv_path = Path("/Users/jonazho/Documents/GitHub/JobHive/ENV's/API.env")
+
+# Load .env file from the specified path
+load_dotenv(dotenv_path=dotenv_path)
 
 client = OpenAI(
-    api_key=os.environ.get("OPENAI_API_KEY"),  # This is the default and can be omitted
+   api_key = os.getenv("AIKey")
 )
 
 chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "Say this is a test",
-        }
-    ],
-    model="gpt-4o",
+   messages=[
+      {
+         "role": "user",
+         "content": "Say this is a test",
+      }
+   ],
+   model="gpt-4o"
 )
+
+
+# Print to check if it's loaded
+
